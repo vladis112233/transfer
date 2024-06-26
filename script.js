4e744ee16835bdefd5943f79efc4642269d39427
vladis112233 <124692818+vladis112233@users.noreply.github.com>
2024-06-26 16:03:04 +0300

    17

--- script.js
+++ script.js
@@ -57,3 +57,23 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scrolled');
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.link-transfer');
+
+    for (const link of links) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    }
+});
--- send_mail.php
+++ send_mail.php
@@ -3,12 +3,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $from = htmlspecialchars($_POST['from']);
     $to = htmlspecialchars($_POST['to']);
+    $phone = htmlspecialchars($_POST['phone']);
     $date = htmlspecialchars($_POST['date']);
     $passengers = htmlspecialchars($_POST['passengers']);
 
-    $to_email = 'grachev01112000@gmail.com'; // замените на свой email
+    $to_email = 'italcar80@gmail.com'; // замените на свой email
     $subject = 'Новая анкета';
-    $message = "Имя: $name\nОткуда: $from\nКуда: $to\nДата: $date\nКоличество пассажиров: $passengers";
+    $message = "Имя: $name\nОткуда: $from\nКуда: $to\nТелефон: $phone\nДата: $date\nКоличество пассажиров: $passengers";
     $headers = 'From: noreply@example.com'; // замените на нужный email отправителя
 
     if (mail($to_email, $subject, $message, $headers)) {
